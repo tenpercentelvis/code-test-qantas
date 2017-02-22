1753e4113b9ef4fc3a280868476cd1f5bb7524b5
Peter Butler <ten.percent.elvis@gmail.com>
2017-02-22 08:17:53 +0700

    Add unit tests

--- app/views/example/component.test.js
+++ app/views/example/component.test.js
@@ -5,7 +5,46 @@ import { mount, shallow } from 'enzyme';
 
 import Example from './component';
 
-describe('Example view', () => {
+describe('Movies', () => {
 
+  let component;
+
+  beforeEach(() => {
+
+    component = shallow(
+      <Example
+        actions={{
+          getData: () => {}
+        }}
+        data={{
+          items: [
+            {'genres': ['Action', 'Adventure']},
+            {'genres': ['Animation', 'Adventure']},
+          ]
+        }}
+      />
+    );
+
+  });
+
+  it('Filter data', () => {
+
+    const instance = component.instance();
+
+    // set
+    // act
+    // verify
+
+  });
+
+  it('Set genres', () => {
+
+    const instance = component.instance();
+
+    // set
+    // act
+    // verify
+
+  });
 
 });
